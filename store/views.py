fb87b8a4f144c571ae27a1183d1f2a3c2142a1f0
Van-Excel <vanexcel.investments@gmail.com>
2024-05-21 20:59:41 +0000

    templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Products
+
 
 # Create your views here.
+
+
+def all_products(request):
+    
+    #It is an SQL query. SELECT * from Products table
+    productList = Products.objects.all()
+    
+    return render(request,  'store/home.html', {'product': productList})
